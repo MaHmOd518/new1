cfc07a4b14693ca34bba1d3963cf4121b8bff9a1
Mahmod Jnead <63670524+MaHmOd518@users.noreply.github.com>
2022-03-16 17:35:28 +0200

    Add files via upload

---
+++ precache-manifest.66b7081031f18f3f62ef63967e02fb18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec4abd24b1bd17d51468e8565271b0ae",
+    "url": "/new1/index.html"
+  },
+  {
+    "revision": "0b5696cd2d760e921181",
+    "url": "/new1/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cc8213d2784e643bd3ea",
+    "url": "/new1/static/js/2.3c54202a.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/new1/static/js/2.3c54202a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b5696cd2d760e921181",
+    "url": "/new1/static/js/main.fdf05a12.chunk.js"
+  },
+  {
+    "revision": "243beff72410ce3417fd",
+    "url": "/new1/static/js/runtime-main.b61d830c.js"
+  }
+]);
